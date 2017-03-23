58d42d54a9a42a84bb6474848badecbac730df5d
Phillip Martin <phillip.martin012@gmail.com>
2017-03-23 17:56:28 -0400

    Update ToWav.py

--- ToWav.py
+++ ToWav.py
@@ -3,7 +3,7 @@ import soundfile as sf
 
 if __name__ == "__main__":
 	filename = sys.argv[1]
-	filename = filename[:filename.index('.')] #Remove the extention
+	filename = filename[:filename.index('.')] #Remove the extension
 
 	sig, rate = sf.read(sys.argv[1])
 	sf.write(filename + ".wav", sig, rate)
