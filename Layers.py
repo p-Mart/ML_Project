Fixed several bugs in backpropagation step. Softmax layer with categorical crossentropy loss now works properly.

Layers.py
@@ -49,7 +49,7 @@ class Relu:
 		return outputs
 
 class Softmax:
-
+	'''Note: softmax must be used for categorical crossentropy'''
 	def __init__(self, input_size, nodes):
 		self.input_size = input_size
 		self.nodes = nodes
@@ -62,8 +62,9 @@ class Softmax:
 
 		#Assuming that the input vector x comes in
 		#with a shape of {1 x input_size}
-
+	
 		return softmax(np.dot(self.weights, x.T))
 
 	def derivative(self, x):
-		return (self.output(x)*(1 - self.output(x)))#This MIGHT be wrong
+		#return (self.output(x)*(1 - self.output(x)))#This MIGHT be wrong
+		return 1.

Network.py
@@ -19,9 +19,12 @@ class Network:
 
 	def lossDerivative(self, y, output, func = "squared error"):
 		if (func == "squared error"):
-			return (y - output)
+			return (-(y - output))
 		elif (func == "categorical crossentropy"):
-			return ((output - y) / (output*(1 - output)))
+			derivative = np.array(output)
+			i = list(y).index(1.) #Get the index containing the correct class
+			derivative[i] = (derivative[i] - 1.)
+			return derivative
 
 	def getOutputs(self, sample):
 		outputs = []
@@ -33,7 +36,8 @@ class Network:
 				x = outputs[i]
 			else:
 				outputs.append(self.layers[i].output(x))
-			
+		
+
 			#outputs.append(self.layers[i].output(x))
 			#x = outputs[i]
 
@@ -44,11 +48,17 @@ class Network:
 
 		gradients = []
 		outputs = self.getOutputs(x)
-
+		#Calculate the backwards pass of gradients.
+		#The number of gradients per layer is the number of nodes(excluding
+		#the bias unit) in that layer.
 
 		for i in range(self.depth):
-			gradients.append(np.ones((outputs[i].shape[0] - 1,1)))
-			
+			if(i < self.depth - 1):
+				gradients.append(np.ones((outputs[i].shape[0] - 1,1)))
+			else:
+				#There's no bias unit on the final layer.
+				gradients.append(np.ones((outputs[i].shape[0],1)))
+		
 
 		#Backpropagation algorithm
 		for i in reversed(range(self.depth)):
@@ -56,7 +66,9 @@ class Network:
 			if (i == self.depth - 1):
 				gradients[i] = (self.lossDerivative(y, outputs[i], self.func) *
 								self.layers[i].derivative(outputs[i-1]))
-				#print gradients[i].shape
+				#print y
+				#print outputs[1]
+				#print gradients[i]
 
 			#Gradients computed backwards from the output layer to the input layer
 			elif(i < self.depth - 1 and i > 0):
@@ -88,21 +100,24 @@ class Network:
 			outputs = self.getOutputs(x)
 			gradients = self.getGradients(x,y)
 			#x = x.reshape((X.shape[1], 1))
-			
+			#print outputs
+			#print gradients
+
 			for i in range(self.depth):
 				#print self.layers[i].weights.shape
 				#print i
 				if (i == 0):
-					self.layers[i].weights =self.layers[i].weights + (self.learning_rate*
+					self.layers[i].weights =self.layers[i].weights - (self.learning_rate*
 										np.outer(gradients[i],x.T))
 				#elif (i > 0 and i < self.depth - 1):
 				#	self.layers[i].weights =self.layers[i].weights + (self.learning_rate*
 				#						np.outer(gradients[i],outputs[i-1]))
 				else:
-					self.layers[i].weights =self.layers[i].weights + (self.learning_rate*
+					self.layers[i].weights =self.layers[i].weights  - (self.learning_rate*
 										np.outer(gradients[i],outputs[i-1]))
 
-		
+			#print self.layers[0].weights
+			#print self.layers[1].weights
 
 	def predict(self, X, Y):
 
@@ -114,8 +129,8 @@ class Network:
 			x = X[i,:]
 			outputs[i] = self.getOutputs(x)[self.depth-1]
 		
-		loss = self.lossFunction(Y, outputs, self.func)
-		print "Loss:", loss
+		#loss = self.lossFunction(Y, outputs, self.func)
+		#print "Loss:", loss
 		return outputs
 
 if __name__ == "__main__":

Nodes.py
@@ -13,4 +13,6 @@ def softmax(x):
 	#Note the x - np.max(x).
 	#This is for numerical stability in case np.exp encounters
 	#large values of x.
-	return np.exp(x - np.max(x)) / np.sum(np.exp(x))
+	
+	f = np.exp(x - np.max(x))
+	return f / np.sum(f) #Return normalized probabilities

network_test.py
@@ -9,35 +9,46 @@ def binary(x):
 	return 1. if x >= 0.5 else 0.
 
 #Generate 100 random samples on the unit square
-n_samples = 100
+n_samples = 800
 X = np.random.uniform(0,1,size=(n_samples,2))
-
+Y = np.zeros((n_samples, 2))
 #Given parameters for the circle on the unit square
 a = 0.5
 b = 0.6
 r = 0.4
-Y = (((X[:,0] - a)**2 + (X[:,1] - b)**2) < r**2)
+y = (((X[:,0] - a)**2 + (X[:,1] - b)**2) < r**2)
+for i in range(len(y)):
+	if (y[i] == 0):
+		Y[i,0] = 1.
+	else:
+		Y[i,1] = 1.
 
 #Test set of 100 samples
 X_test = np.random.uniform(0,1,size=(n_samples,2))
-Y_test = (((X_test[:,0] - a)**2 + (X_test[:,1] - b)**2) < r**2)
+y_test = (((X_test[:,0] - a)**2 + (X_test[:,1] - b)**2) < r**2)
+Y_test = np.zeros((n_samples, 2))
+for i in range(len(y_test)):
+	if (y_test[i] == 0):
+		Y_test[i,0] = 1.
+	else:
+		Y_test[i,1] = 1.
 
 #Hyperparameters
 hidden_nodes = 10
 output_nodes = 1
-learning_rate = 1
-number_epochs = 20000
+learning_rate = 0.03
+number_epochs = 40000
 
 
 colors = ['red','blue']
 plt.figure(figsize=(8,8))
-plt.scatter(X_test[:,0],X_test[:,1],c=Y_test,cmap=matplotlib.colors.ListedColormap(colors))
+plt.scatter(X_test[:,0],X_test[:,1],c=y_test,cmap=matplotlib.colors.ListedColormap(colors))
 plt.xlabel(r'$x_1$')
 plt.ylabel(r'$x_2$')
 plt.title("Training Set")
 
 layer_1 = Relu(X.shape[1] , hidden_nodes)
-layer_2 = Softmax(hidden_nodes, 1)
+layer_2 = Softmax(hidden_nodes, 2)
 
 model = Network([layer_1, layer_2],
 					learning_rate = learning_rate,
@@ -46,12 +57,13 @@ model = Network([layer_1, layer_2],
 
 model.train(X, Y, number_epochs)
 outputs = model.predict(X_test, Y_test)
-print outputs
-for i in range(len(outputs)):
-	outputs[i] = binary(outputs[i])
+#print outputs
+maxes = outputs.argmax(axis = 1)
+
+#print prediction
 
 plt.figure(figsize=(8,8))
-plt.scatter(X_test[:,0],X_test[:,1],c=outputs,cmap=matplotlib.colors.ListedColormap(colors))
+plt.scatter(X_test[:,0],X_test[:,1],c=maxes,cmap=matplotlib.colors.ListedColormap(colors))
 plt.xlabel(r'$x_1$')
 plt.ylabel(r'$x_2$')
 plt.title("Training Set")