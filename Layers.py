27ebe608b229de8bd08fc9210ca796a7d7391729
p-Mart <phillip.martin012@gmail.com>
2017-03-25 00:56:15 -0400

    Added max pool layer.

--- Layers.py
+++ Layers.py
@@ -68,3 +68,33 @@ class Softmax:
 	def derivative(self, x):
 		#return (self.output(x)*(1 - self.output(x)))#This MIGHT be wrong
 		return 1.
+
+class MaxPool:
+
+	def __init__(self, receptive_field, stride):
+		self.f = receptive_field
+		self.s = stride
+
+	def maxPool(self, x):
+		'''Assuming x is some tensor of shape [h x w x d]'''
+		f = self.f #Size of partitions being looked at
+		s = self.s #Stride over the input
+		
+		#Compute the output dimensions of the max pool layer
+		h, w, d = x.shape
+		output_h = (h - f)/s + 1
+		output_w = (w - f)/s + 1
+		output_d = d
+
+		pool = np.empty((output_h,output_w,output_d))
+		#This could probably be significantly optimized	
+		#Max pooling operation
+		for k in range(d):
+			for i in range(0,h-f+1,s):
+				for j in range(0,w-f+1,s):
+					pool[i/s,j/s,k] = np.max(x[i:(i+f),j:(j+f),k])
+
+		return pool
+
+
+
