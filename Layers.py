Fixed backprop through convolutional network. Can stack multiple layers now.

Layers.py
@@ -1,6 +1,6 @@
 import numpy as np
 from Nodes import *
-from scipy.signal import convolve2d
+from scipy.signal import convolve2d, convolve
 
 class Sigmoid:
 
@@ -209,26 +209,46 @@ class Convolutional:
 
 		out = np.empty(self.output_shape)
 		for i in range(self.k):
-			w = self.weights[i, 1:].reshape((self.f, self.f))
-			out[:,:,i] = (convolve2d(x[:,:,0], w, mode="valid")
-					+ self.weights[i, 0] * 1.)
+			w = self.weights[i, 1:].reshape((self.f, self.f, self.input_shape[2]))
+			out[:,:,i] = (relu(convolve(x, w, mode="valid"))[:,:,0]
+					+ relu(self.weights[i, 0] * 1.))
 
 		self.outputs = out
 		return out
 
 	def derivative(self, x):
-		d = np.array(self.outputs).flatten()
-		d = d.reshape((len(d), 1))
-		return d
+		
+		outputs = np.array(self.outputs).flatten()
+		for i in range(len(outputs)):
+			if(outputs[i] <= 0):
+				outputs[i] = 0.
+			else:
+				outputs[i] = 1.
+
+		return outputs.reshape((len(outputs), 1))
+		
+	def gradient(self, grads):
+		grads = grads.reshape(self.output_shape)
+		gradient = np.zeros(self.input_shape)
+		for i in range(self.k):
+			w = self.weights[i, 1:].reshape((self.f, self.f, self.input_shape[2]))
+			w = np.rot90(w, 2)
+			g = grads[:,:, i].reshape(self.h, self.w, 1)
+			gradient = gradient + convolve(g, w)
+
+		return gradient
+
 
 	def weightUpdate(self, grads, x):
 		grad = grads.reshape(self.output_shape)
 		grad = np.rot90(grad, 2)
 		x = x.reshape(self.input_shape)
-
+		#print "Weight shape", self.weights.shape
+		#print "Input shape", x.shape
+		#print "Gradient shape", grad.shape
 		d_weights = np.empty(self.weights.shape)
 		for i in range(self.k):
-			filter = convolve2d(x[:,:,0], grad[:,:,i],mode="valid")
+			filter = convolve(x, grad[:,:,i].reshape(self.h, self.w, 1),mode="valid")
 			d_weights[i, 1:] = filter.flatten()
 			d_weights[i, 0] = np.sum(filter)
 

Network.py
@@ -79,20 +79,30 @@ class Network:
 			#Gradients computed backwards from the output layer to the input layer
 			elif(i < self.depth - 1 and i > 0):
 				#print i, self.layers[i+1].weights.T[1:,:].shape
-				#print gradients[i+1].shape
+				#print i,  [i].shape
 				#print np.count_nonzero(np.dot(self.layers[i+1].weights.T[1:,:], gradients[i+1]))
 				#print self.layers[i].derivative(self.outputs[i-1].flatten()).shape
 				#print self.outputs[i-1].flatten().shape
-				gradients[i]  = (np.dot(self.layers[i+1].weights.T[1:,:], gradients[i+1]) * 
-								self.layers[i].derivative(self.outputs[i-1].flatten()))
+				if(self.layers[i+1].__class__.__name__ == "Convolutional"):
+					gradients[i] = (self.layers[i+1].gradient(gradients[i+1]).flatten()
+									* self.layers[i].derivative(self.outputs[i-1].flatten()))
+				else:					
+					gradients[i]  = (np.dot(self.layers[i+1].weights.T[1:,:], gradients[i+1])  
+									* self.layers[i].derivative(self.outputs[i-1].flatten()))
+				
+				gradients[i] = gradients[i].reshape((gradients[i].shape[0], 1))
+
+				#print gradients[i].shape
 			else:
 				#print i, self.layers[i+1].weights.shape
 				#print outputs[i].shape
-				#print gradients[i].shape
+				#print i, gradients[i].shape
 				#print gradients[i+1].shape
 				#print self.layers[i+1].weights.shape
 				gradients[i]  = (np.dot(self.layers[i+1].weights.T[1:,:], gradients[i+1]) * 
 								self.layers[i].derivative(x.flatten()))
+
+				#print gradients[i].shape
 				
 
 		return gradients
@@ -158,13 +168,10 @@ class Network:
 					if(self.layers[i].__class__.__name__ == "MaxPool"):
 						continue
 					elif(self.layers[i].__class__.__name__ == "Convolutional"):
-						temp_weights = (self.learning_rate*np.outer(gradients[i],
-							 	self.layers[i].im2col(outputs[i-1]).flatten()
-							 	)
-							 )
+						dweights = (self.learning_rate 
+								* self.layers[i].weightUpdate(gradients[i], self.outputs[i-1].flatten()))
 
-						weights = self.layers[i].weightUpdate(temp_weights)
-						self.layers[i].weights = self.layers[i].weights - weights
+						self.layers[i].weights = self.layers[i].weights - dweights
 					else:
 						self.layers[i].weights = (self.layers[i].weights  -
 							 self.learning_rate*np.outer(gradients[i],
@@ -179,7 +186,7 @@ class Network:
 			if(loss < best_loss):
 				best_loss = loss
 		
-		print y	
+		#print y	
 		sys.stdout.write("\nBest Loss: %f\n" % (best_loss))
 		sys.stdout.flush()
 

Nodes.py
@@ -14,6 +14,6 @@ def softmax(x):
 	#This is for numerical stability in case np.exp encounters
 	#large values of x.
 	f = np.exp(x - np.max(x))
-	print f
+	#print f
 	#print f / np.sum(f)
 	return f / np.sum(f) #Return normalized probabilities

network_test_mnist.py
@@ -22,28 +22,38 @@ X_train = train_set[0]
 Y_train = toLogit(train_set[1])
 
 #Hyperparameters
-n_filters = 5
+#n_filters = 8
 n_classes = 10
 
 layer_1 = Convolutional(input_shape=(28,28,1),
-					number_filters=n_filters,
-					spatial_extent=3,
+					number_filters=8,
+					spatial_extent=5,
 					stride=1,zero_padding=0)
 #layer_1_os = layer_1.output(x).shape
 #print layer_1.weights.shape
+#print layer_1.output_shape
 
-layer_2 = Relu(layer_1.output_shape, np.prod(np.array(layer_1.output_shape)))
-#layer_2_os = layer_2.output(layer_1.output(x)).shape
-#print layer_2.weights.shape
-layer_3 = MaxPool(layer_1.output_shape,2,2)
-#layer_3_os = layer_3.output(layer_2.output(layer_1.output(x))).shape
+layer_2 = MaxPool(input_shape=layer_1.output_shape,
+					receptive_field=2,
+					stride=2)
+#print layer_2.output_shape
+layer_3 = Convolutional(input_shape=layer_2.output_shape,
+					number_filters=16,
+					spatial_extent=5,
+					stride=1,
+					zero_padding=0)
+#print layer_3.output_shape
+#print layer_3.weights.shape
+layer_4 = MaxPool(input_shape=layer_3.output_shape,
+					receptive_field=3,
+					stride=3)
 
-layer_4 = Softmax(layer_3.output_shape, n_classes)
-#layer_4_os = layer_4.output(layer_3.output(layer_2.output(layer_1.output(x))))
-#print layer_4.weights.shape
+layer_5 = Softmax(layer_4.output_shape, n_classes)
+
+
 model = Network(
-				[layer_1, layer_2,layer_3,layer_4],
-				learning_rate = 0.02,
+				[layer_1, layer_2,layer_3,layer_4, layer_5],
+				learning_rate = 0.01,
 				func = "categorical crossentropy"
 				)
 
@@ -56,14 +66,22 @@ Y_valid = toLogit(valid_set[1])
 
 predictions = model.predict(X_valid[:10,:], Y_valid[:10,:])
 conv_layers_visual = model.outputs[0][:,:,0]
-print model.outputs[3]
-for i in range(n_filters-1):
+#print model.outputs[3]
+for i in range(7):
 	conv_layers_visual = np.concatenate(
 							(conv_layers_visual, model.outputs[0][:,:,i+1]),
 							axis = 1)
 
+
+
 plt.imshow(conv_layers_visual)
 plt.show()
+for i in range(predictions.shape[0]):
+	max_index = np.argmax(predictions[i, :])
+	predictions[i, :] = np.zeros((1, predictions.shape[1]))
+	predictions[i, :][max_index] = 1.
+
 print predictions
+print "\n"
 print Y_valid[:10,:]
 