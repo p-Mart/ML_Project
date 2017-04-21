e0f48af489eeed17ae4c68f7018841d6ab5ca4ea
cesar <cesar.atteh@gmail.com>
2017-04-20 22:02:21 -0400

    cleaned up code in Main-store-dict-file to make it faster

--- input-handling/Main-store-dict-file.py
+++ input-handling/Main-store-dict-file.py
@@ -6,10 +6,8 @@ directory = '/home/genous/Downloads/LibriSpeech/dev-clean/'
 
 wordIndex = 0
 wordToIndex = {}
-indexToWord = {}
 
-wordToIndexFile = open(directory + "/" + "wordToIndex", "w+")
-#indexToWordFile = open(directory + "/" + "indexToWord", "w+")
+wordToIndexFile = open(directory + "/" + "Word_To_Index_Cache", "w+")
 
 for filename in os.listdir(directory):
 	
@@ -24,29 +22,9 @@ for filename in os.listdir(directory):
 				    if filename3 == "cache-file":
 				    	with open(os.path.join(directory, filename, filename2, filename3), 'rd') as f:
 				    		for line in f:
-				    			audioFilePath = os.path.join(directory, filename, filename2, line.split(' ')[0])
-
 				    			word = line.split(' ')[1]
-				    			
-				    			startTime = float(line.split(' ')[2])
-				    			endTime = float(line.split(' ')[3])
+			    				print(word)
 
-				    			with open(audioFilePath, 'rd') as a:
-				    				timelinedWord = WordWithTimeline(word, startTime, endTime)
-				    				
-			    					features = FeaturesExtractor.getFeatures(timelinedWord, audioFilePath)
-			    					#print(audioFilePath)
-			    					print(word)
-			    					#print("Start time " + str(startTime))
-				    				#print("End time " + str(endTime))
-			    					#print("Feature size: " + str(features.size))
-
-			    					if word not in wordToIndex:
-				    					wordToIndex[word] = wordIndex
-				    					indexToWord[wordIndex] = word
-				    					wordIndex += 1
-				    					wordToIndexFile.write(word + "\n")
-
-
-			    					#!!!!!!!!!!!!!!!!!!!!!!! Do SOMETING WITH FEATURES HERE !!!!!!!!!!!!!!!!!!!!!!!
-			    					# They have a variable size so we probably need to do something about that
+			    				if word not in wordToIndex:
+				    				wordToIndex[word] = wordIndex
+				    				wordToIndexFile.write(word + "\n")
