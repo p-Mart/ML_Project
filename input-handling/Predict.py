5a6119c2befd86235a7c16c49f4e2c402693debf
cesar <cesar.atteh@gmail.com>
2017-04-21 12:57:42 -0400

    predict now runs on the dataset

--- input-handling/Predict.py
+++ input-handling/Predict.py
@@ -9,6 +9,7 @@ import MainStoreDictFile
 import os
 
 feature_length = 625
+data_size = 1000
 
 
 #directory = '/home/dev-clean/'
@@ -38,6 +39,58 @@ def load_dictionary_to_global_variable():
 
 word_to_index, index_to_word, size_of_output_labels_vector = load_dictionary_to_global_variable()
 
+
+def initialize():
+	index = 0
+	
+	all_features = np.empty((data_size, feature_length))
+	all_output_labels = np.empty((data_size, size_of_output_labels_vector))  #  loop through all words, call word_to_index[word] and assign it to np.zeros
+	all_words = {}
+
+	for filename in os.listdir(directory):
+		
+		if os.path.isdir(directory + "/" + filename):
+			
+			for filename2 in os.listdir(os.path.join(directory, filename)):
+				
+				if os.path.isdir(directory + "/" + filename + "/" + filename2):
+					
+					for filename3 in os.listdir( os.path.join(directory, filename, filename2)):
+					    
+					    if filename3 == "cache-file":
+					    	with open(os.path.join(directory, filename, filename2, filename3), 'rd') as f:
+					    		for line in f:
+
+					    			audioFilePath = os.path.join(directory, filename, filename2, line.split(' ')[0])
+					    			word = line.split(' ')[1]
+					    			startTime = float(line.split(' ')[2])
+					    			endTime = float(line.split(' ')[3])
+
+					    			with open(audioFilePath, 'rd') as a:
+					    				timelinedWord = WordWithTimeline(word, startTime, endTime)
+					    				
+					    				try:
+					    					all_features[index, :] = FeaturesExtractor.getFeaturesFFT(timelinedWord, audioFilePath, feature_length)
+					    					all_output_labels[index, :] = np.zeros(size_of_output_labels_vector)
+					    					
+					    					all_words[index] = word
+
+					    					all_output_labels[index, word_to_index[word]] = 1
+					    					index += 1
+
+					    				except ValueError:
+					    					print("skipping word, all zeros")
+
+				    					if index >= data_size:
+				    						return all_output_labels, all_features, all_words
+
+
+all_output_labels, all_features, all_words = initialize()
+
+print("DONE INITIALIZING")
+
+
+n_examples = data_size
 n_classes = size_of_output_labels_vector
 	
 x = int(np.sqrt(feature_length))
@@ -68,4 +121,5 @@ for i in range(predictions.shape[0]):
 	predictions[i, :] = np.zeros((1, predictions.shape[1]))
 	predictions[i, :][max_index] = 1.
 
-	print index_to_word[max_index],
+	print(index_to_word[max_index] + " | " + all_words[i])
+	
